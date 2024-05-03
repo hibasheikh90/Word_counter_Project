0fd8486eafb82e719f3802c8004d5169a3b4e328
hibasheikh90 <hayasheikh109@gmail.com>
2024-05-03 16:53:22 +0500

    word counter project

---
+++ index.js
@@ -0,0 +1,13 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+console.log("Welcome To Hiba Sheikh Word Counter Project");
+const answer = await inquirer.prompt([
+    {
+        name: "sentence",
+        type: "input",
+        message: "Enter Your Sentence To count The Word."
+    }
+]);
+const words = answer.sentence.trim().split(" ");
+console.log(words);
+console.log(`Your Sentence Count Word is ${words.length}`);
